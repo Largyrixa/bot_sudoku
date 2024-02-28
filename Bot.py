a36fedf09e495f3999242de4f47150f2147e6ca0
Largyrixa <gartictasemnick@gmail.com>
2024-02-28 20:11:35 -0300

    Adicionado a porcentagem de profundidade da função recursiva.

---
+++ Bot.py
@@ -0,0 +1,136 @@
+import os, time
+
+def resolver(grid, p=0):
+    """
+    Resolve o quebra-cabeça de Sudoku usando uma abordagem iterativa para reduzir o tempo de uso da função recursiva.
+    
+    Parameters:
+        grid (list): A grade de Sudoku como uma lista de listas.
+        p (int): Profundidade atual na recursão (padrão é 0).
+    
+    Returns:
+        bool: True se o quebra-cabeça foi resolvido, False caso contrário.
+    """
+    i = 0
+    while i < 9:
+        j = 0
+        while j < 8:
+            j += 1
+            j %= 9
+            if grid[i][j] == 0:
+                vizinhos = get_vizinhos(grid, i, j)
+                possiveis = [x for x in range(1, 10) if x not in vizinhos]
+                mostrar(grid, i, j, possiveis[:], p=p)
+                if len(possiveis) > 1:
+                    continue
+                if len(possiveis) == 1:
+                    grid[i][j] = possiveis[0]
+                    i = 0
+                    j = -1
+        i += 1
+        j %= 9
+    if resolvido(grid):
+        return True
+    return resolver_recursivo(grid, p=p)
+
+def resolver_recursivo(grid, i=0, j=0, p=0):
+    """
+    Resolve o quebra-cabeça de Sudoku usando uma abordagem recursiva.
+    
+    Parameters:
+        grid (list): A grade de Sudoku como uma lista de listas.
+        i (int): Índice da linha atual (padrão é 0).
+        j (int): Índice da coluna atual (padrão é 0).
+        p (int): Profundidade atual na recursão (padrão é 0).
+    
+    Returns:
+        bool: True se o quebra-cabeça foi resolvido, False caso contrário.
+    """
+    if j == 9:
+        if i == 8:
+            return True
+        j = 0
+        i += 1
+
+    if grid[i][j] > 0:
+        return resolver_recursivo(grid, i, j + 1, p + 1)
+
+    for num in range(1, 10):
+        if is_valid_move(grid, i, j, num):
+            grid[i][j] = num
+            if resolver_recursivo(grid, i, j + 1, p + 1):
+                return True
+        mostrar(grid, i, j, num, p=p)
+        grid[i][j] = 0
+
+    return False
+
+def mostrar(grid, i=-1, j=-1, num=None, p=81):
+    """
+    Exibe a grade de Sudoku com destaque na célula atual e nas informações relevantes.
+    
+    Parameters:
+        grid (list): A grade de Sudoku como uma lista de listas.
+        i (int): Índice da linha atual (padrão é -1).
+        j (int): Índice da coluna atual (padrão é -1).
+        num (int): Número analisado (padrão é None).
+        p (int): Profundidade atual na recursão (padrão é 81).
+    """
+    if num is not None:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(f'Célula analisada: ({i},{j})')
+        print(f'Número(s) analisado(s): {num}')
+        print(f'Profundidade: {p} ({p*100/81})')
+    for x in range(9):
+        if x % 3 == 0 and x != 0:
+            print('--- + --- + ---')
+        for y in range(9):
+            print(f'\033[1;31m{grid[x][y]}\033[m' if x == i and y == j else grid[x][y],
+                  end=' | ' if (y + 1) % 3 == 0 and y != 0 and y != 8 else '')
+        print()
+    time.sleep(1 / 59)
+
+def is_valid_move(grid, i, j, num):
+    for x in grid[i]:
+        if x == num:
+            return False
+    
+    for x in range(9):
+        if grid[x][j] == num:
+            return False
+    i_i = i - i % 3
+    i_j = j - j % 3
+    celula = [grid[i_i + x][i_j + y] for y in range(3) for x in range(3)]
+    
+    for x in celula:
+        if x == num:
+            return False
+    return True
+
+def resolvido(grid):
+    for x in range(9):
+        soma = 0
+        for y in range(9):
+            soma += grid[x][y]
+        if soma != 45: return False
+    return True
+
+def get_vizinhos(grid,i,j):
+    viz = [x for x in grid[i] if x != 0]
+    viz.extend(grid[x][j] for x in range(9))
+    i_i = i - i % 3
+    i_j = j - j % 3
+    celula = [grid[i_i + x][i_j + y] for y in range(3) for x in range(3)]
+    for l in celula:
+        viz.append(l)
+    i = 0
+    while len(viz) > 0:
+        if viz[0] == 0:viz.pop(0)
+        imax = len(viz)
+        if viz[i] == 0:
+            viz.pop(i)
+            i = -1
+        i += 1
+        if imax == i:break
+    return viz
+    
---
+++ main.py
@@ -0,0 +1,97 @@
+import os
+import time
+import random
+import copy
+from Bot import resolver, mostrar, resolvido
+
+def gerar_sudoku():
+    """
+    Gera um quebra-cabeça de Sudoku aleatório removendo alguns números de uma grade resolvida.
+    
+    Returns:
+        list: Grade de Sudoku gerada.
+    """
+    # Inicializa um Sudoku resolvido
+    solved_sudoku = [
+        [5, 3, 4, 6, 7, 8, 9, 1, 2],
+        [6, 7, 2, 1, 9, 5, 3, 4, 8],
+        [1, 9, 8, 3, 4, 2, 5, 6, 7],
+        [8, 5, 9, 7, 6, 1, 4, 2, 3],
+        [4, 2, 6, 8, 5, 3, 7, 9, 1],
+        [7, 1, 3, 9, 2, 4, 8, 5, 6],
+        [9, 6, 1, 5, 3, 7, 2, 8, 4],
+        [2, 8, 7, 4, 1, 9, 6, 3, 5],
+        [3, 4, 5, 2, 8, 6, 1, 7, 9]
+    ]
+
+    # Cria uma cópia da grade resolvida do Sudoku
+    sudoku_puzzle = [row[:] for row in solved_sudoku]
+
+    # Define o número de células a serem removidas para um quebra-cabeça de dificuldade média
+    cells_to_remove = random.randint(30, 60)
+
+    # Remove números para criar o quebra-cabeça
+    for _ in range(cells_to_remove):
+        row, col = random.randint(0, 8), random.randint(0, 8)
+        sudoku_puzzle[row][col] = 0
+
+    return sudoku_puzzle
+input('pressione enter para começar...')
+tabuleiro = [[8,9,4,0,0,0,0,5,1],
+             [0,0,7,0,0,3,0,6,9],
+             [0,6,0,5,0,4,0,0,0],
+             [0,3,8,4,5,1,0,0,0],
+             [2,0,0,0,0,6,8,0,5],
+             [6,0,0,0,0,2,7,0,0],
+             [3,8,0,1,7,5,0,0,0],
+             [4,0,0,3,6,9,1,0,8],
+             [0,1,0,0,0,0,5,7,0]]
+'''tabuleiro=[[0,0,5,3,0,0,0,0,0],
+                   [8,0,0,0,0,0,0,2,0],
+                    [0,7,0,0,1,0,5,0,0],
+                    [4,0,0,0,0,5,3,0,0],
+                    [0,1,0,0,7,0,0,0,6],
+                    [0,0,3,2,0,0,0,8,0],
+                    [0,6,0,5,0,0,0,0,9],
+                    [0,0,4,0,0,0,0,3,0],
+                    [0,0,0,0,0,9,7,0,0]]'''
+tentativas = 0
+l_tentativas = []
+acertos = 0
+porc = 0
+foi = []
+while True:
+    
+    tabuleiro = gerar_sudoku()
+    
+    #tabuleiro = [[0 for _ in range(9)]for _ in range(9)]
+    time_i = time.time()
+    l_tentativas.append(copy.deepcopy(tabuleiro))
+    l_tentativas.append(tabuleiro)
+    tentativas += 1
+    tabuleiro_bot = tabuleiro
+    resolver(tabuleiro_bot)
+    if resolvido(tabuleiro_bot):
+        acertos += 1
+    os.system('cls' if os.name == 'nt' else 'clear')
+    porc = acertos*100/tentativas
+    print(f'Resorvido: {resolvido(tabuleiro_bot)}')
+    print(f'Jogo: {tentativas}/1000')
+    print(f'Tempo: {-time_i + time.time()}')
+    print(f'Porcentagem de acertos: {porc}%')
+    mostrar(tabuleiro_bot, tentativas)
+    time.sleep(2)
+    if tentativas == 1000:
+        max_tentativas = 1000
+        break
+print(porc)
+i = 1
+while i != max_tentativas*2 + 1:
+    input('pressione enter para continuar')
+    print(i)
+    print(foi[(i-1)//2])
+    tabuleiro = l_tentativas[i-1:i+1]
+    mostrar(tabuleiro[0])
+    mostrar(tabuleiro[1])
+    i+=2
+input()
